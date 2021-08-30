8e5bec013bb696bae6919dcd0e36310c5c94c46d
Trivo25 <trivoplays@gmail.com>
2021-08-31 00:29:04 +0200

    added icon

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -13,7 +13,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Minaview - an ecosystem overview of the Mina Protocol',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],

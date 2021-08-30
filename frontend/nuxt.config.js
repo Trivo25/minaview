b087febcb6617f894da975f54aa546e85df92bf4
Trivo25 <trivoplays@gmail.com>
2021-08-30 14:52:40 +0200

    changed configs

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
+  target: 'static',
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 

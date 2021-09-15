9a5181460fe5421e0197c83031692d439d895d42
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2021-09-15 23:01:57 +0200

    Update project.vue

--- frontend/pages/project.vue
+++ frontend/pages/project.vue
@@ -1,17 +1,41 @@
 <template>
-  <div  style="left: 500px;">{{getServiceHash()}}</div>
+  <div class="project-wrapper">
+    <div class="header">
+      <div class="logo">
+        <img :src="service.ServiceLogo" alt="Project Logo">
+      </div>
+      <div class="title">
+        {{ service.ServiceName }}
+      </div>
+    </div>
+    <div class="socials">
+      scials
+    </div>
+    <div class="content">
+      content
+    </div>
+    <Error @closeNotification="error.show = false" v-if="error.show" :error="error.error" :type="error.type" />
+  </div>
 </template>
 
 <script>
+import Error from "../components/Error.vue"
 
 export default {
   name: "project",
   layout: "project-page",
   props: [],
   components: {
+    Error
   },
   data () {
     return {
+      service: new Object,
+      error: {
+        show: false,
+        error: null,
+        type: "error"
+      }
     }
   },
   methods: {
@@ -26,23 +50,10 @@ export default {
         let res = await this.$axios.post("/getServiceByHash", {
           Hash: this.getServiceHash(),
         })
-        if(res.data.Error != undefined) {
-          this.error.error = {
-            Error: res.data.Error,
-            Code: ""
-          }
-          this.error.show = true
-          this.error.type = "error"
-        } else {
-          this.error.error = {
-            Error: "Your request has been sent!",
-            Code: ""
-          }
-          this.error.show = true
-          this.error.type = "success"
-        }
+
+        this.service = res.data.Data
     } catch (error) {
-      
+      console.log(error)
     }
   }
 }

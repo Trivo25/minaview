11cf97fbcaff18833e15ce7b0f22b07115b8dc54
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2021-11-06 21:50:44 +0100

    faq layout

---
+++ frontend/components/Drawer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>Test</span>
+    <v-btn
+      icon
+      @click="show = !show"
+    >
+      <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+    </v-btn>
+    <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Drawer",
+  props: ["title", "content"],
+  data() {
+    return {
+     show: false 
+    }
+  },
+  methods: {}
+}
+</script>
---
+++ frontend/pages/faq.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="faq">
+    <h1 class="faq-title">Frequently Asked Questions</h1>
+    <Drawer/>
+  </div>
+</template>
+
+<script>
+import Drawer from "../components/Drawer.vue"
+
+export default {
+  name: "faq",
+  layout: "faq-layout",
+  props: [],
+  components: {
+    Drawer
+  },
+}
+</script>
+
+
+<style scoped>
+
+.faq {
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  height: 600px !important;
+}
+
+.faq-title {
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: grey;
+
+}
+
+</style>
